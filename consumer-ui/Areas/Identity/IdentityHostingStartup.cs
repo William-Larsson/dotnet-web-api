38ccfad17d71104b7cb00b50cd0415e16e950c42
William Larsson <williaamlarsson@gmail.com>
2021-04-03 07:43:47 +0200

    Added identity pages through scaffolding

---
+++ consumer-ui/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+using System;
+using consumer_ui.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(consumer_ui.Areas.Identity.IdentityHostingStartup))]
+namespace consumer_ui.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
